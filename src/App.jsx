f7715242f382be4bbdc420ee26eb7596d9dd9b89
bdstn10 <budistn108@gmail.com>
2024-02-11 07:43:52 +0700

    Use components from external file sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-// Di bawah merupakan contoh penggunaan component di React
-
-function Image() {
-  return (
-    <img src="https://akcdn.detik.net.id/visual/2020/08/13/darth-vader_43.jpeg?w=650&q=90" alt="Gambar Darth Vader" />
-  )
-}
-
-function Gallery() {
-  return (
-    <section>
-      <h1>Selamat Datang di Gallery Kami</h1>
-      <Image/>
-    </section>
-  )
-}
+// Contoh penggunaan component dengan mengimport dari file external
+import Gallery from "./Gallery"
 
 function App() {
   return (
---
+++ src/Gallery.jsx
@@ -0,0 +1,14 @@
+function Image() {
+    return (
+      <img src="https://akcdn.detik.net.id/visual/2020/08/13/darth-vader_43.jpeg?w=650&q=90" alt="Gambar Darth Vader" />
+    )
+  }
+  
+export default function Gallery() {
+    return (
+      <section>
+        <h1>Selamat Datang di Gallery Kami</h1>
+        <Image/>
+      </section>
+    )
+  }
