07265f38ff5af8e802a88a87dcf953eda456af2c
bdstn10 <budistn108@gmail.com>
2024-02-11 07:05:10 +0700

    Standard usage of React's Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// Di bawah merupakan contoh penggunaan component di React
 
+function Image() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <img src="https://akcdn.detik.net.id/visual/2020/08/13/darth-vader_43.jpeg?w=650&q=90" alt="Gambar Darth Vader" />
+  )
+}
+
+function Gallery() {
+  return (
+    <section>
+      <h1>Selamat Datang di Gallery Kami</h1>
+      <Image/>
+    </section>
+  )
+}
+
+function App() {
+  return (
+    <Gallery/>
   )
 }
 
